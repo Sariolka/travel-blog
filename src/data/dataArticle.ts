8af8e9ce5136d7e1eaadd9acc0658a725c8696f1
Sariolka <sariola@list.ru>
2025-04-08 13:01:47 +0300

    fix: change data

--- src/data/dataArticle.ts
+++ src/data/dataArticle.ts
@@ -1,41 +1,16 @@
 export const translations = {
   en: {
-    description1: "Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh\n" +
-      "          front liner sport detail. Warmth comfort hangs loosely from the body large pocket\n" +
-      "          at&nbsp;the front full button detail cotton blend cute functional. Bodycon skirts bright\n" +
-      "          primary colours punchy palette pleated cheerleader vibe stripe trims. Staple court shoe\n" +
-      "          chunky mid block heel almond toe flexible rubber sole simple chic ideal handmade metallic\n" +
-      "          detail. Contemporary pure silk pocket square sophistication luxurious coral print pocket\n" +
-      "          pattern&nbsp;On trend inspired shades.",
-    description2: " Striking pewter studded epaulettes silver zips inner drawstring waist channel urban edge\n" +
-      "          single-breasted jacket. Engraved attention to detail elegant with neutral colours cheme\n" +
-      "          quartz leather strap fastens with a&nbsp;pin a&nbsp;buckle clasp. Workwear bow detailing\n" +
-      "          a&nbsp;slingback buckle strap stiletto heel timeless go-to shoe sophistication slipper\n" +
-      "          shoe. Flats elegant pointed toe design cut-out sides luxe leather lining versatile shoe\n" +
-      "          must-have new season glamorous.",
-    quote: " Knicker lining concealed back zip fasten swing style high waisted double layer full\n" +
-      "            pattern floral.",
-    description3: "Foam padding in&nbsp;the insoles leather finest quality staple flat slip-on design pointed\n" +
-      "          toe off-duty shoe. Black knicker lining concealed back zip fasten swing style high waisted\n" +
-      "          double layer full pattern floral. Polished finish elegant court shoe work duty stretchy\n" +
-      "          slingback strap mid kitten heel this ladylike design",
+    description1: "Structured gripped tape invisible moulded cups for sauppor firm hold strong powermesh front liner sport detail. Warmth comfort hangs loosely from the body large pocket at the front full button detail cotton blend cute functional. Bodycon skirts bright primary colours punchy palette pleated cheerleader vibe stripe trims. Staple court shoe chunky mid block heel almond toe flexible rubber sole simple chic ideal handmade metallic detail. Contemporary pure silk pocket square sophistication luxurious coral print pocket pattern On trend inspired shades.",
+    description2: "Striking pewter studded epaulettes silver zips inner drawstring waist channel urban edge single-breasted jacket. Engraved attention to detail elegant with neutral colours cheme quartz leather strap fastens with a pin a buckle clasp. Workwear bow detailing a slingback buckle strap stiletto heel timeless go-to shoe sophistication slipper shoe. Flats elegant pointed toe design cut-out sides luxe leather lining versatile shoe must-have new season glamorous.",
+    quote: "Knicker lining concealed back zip fasten swing style high waisted double layer full pattern floral.",
+    description3: "Foam padding in the insoles leather finest quality staple flat slip-on design pointed toe off-duty shoe. Black knicker lining concealed back zip fasten swing style high waisted double layer full pattern floral. Polished finish elegant court shoe work duty stretchy slingback strap mid kitten heel this ladylike design",
     description4: "Eget aenean tellus venenatis. Donec odio tempus. Felis arcu pretium metus nullam quam aenean sociis quis sem neque vici libero. Venenatis nullam fringilla pretium magnis aliquam nunc vulputate integer augue ultricies cras. Eget viverra feugiat cras ut. Sit natoque montes tempus ligula eget vitae pede rhoncus maecenas consectetuer commodo condimentum aenean.",
     h4: "Eu ridiculus fringilla aenean",
-    description5: " Sociis consequat adipiscing sit curabitur donec sem luctus cras natoque vulputate dolor\n" +
-      "          eget dapibus. Nec vitae eros ullamcorper laoreet dapibus mus ac&nbsp;ante viverra.\n" +
-      "          A&nbsp;aenean sit augue curabitur et&nbsp;parturient nisi sed enim. Nulla nec quis sit\n" +
-      "          quisque sem commodo ultricies neque. Lorem eget venenatis dui ante luctus ultricies tellus\n" +
-      "          montes. Quis in&nbsp;sapien tempus.",
+    description5: "Sociis consequat adipiscing sit curabitur donec sem luctus cras natoque vulputate dolor eget dapibus. Nec vitae eros ullamcorper laoreet dapibus mus ac ante viverra. A aenean sit augue curabitur et parturient nisi sed enim. Nulla nec quis sit quisque sem commodo ultricies neque. Lorem eget venenatis dui ante luctus ultricies tellus montes. Quis in sapien tempus.",
     listItem1: "Crisp fresh iconic elegant timeless clean perfume",
     listItem2: "Neck straight sharp silhouette and dart detail",
     listItem3: "Machine wash cold slim fit premium stretch selvedge denim comfortable low waist",
-    description6: "See-through delicate embroidered organza blue lining luxury acetate-mix stretch pleat\n" +
-      "          detailing. Leather detail shoulder contrastic colour contour stunning silhouette working\n" +
-      "          peplum. Statement buttons cover-up tweaks patch pockets perennial lapel collar flap chest\n" +
-      "          pockets topline stitching cropped jacket. Effortless comfortable full leather lining\n" +
-      "          eye-catching unique detail to&nbsp;the toe low &lsquo;cut-away&rsquo; sides clean and\n" +
-      "          sleek. Polished finish elegant court shoe work duty stretchy slingback strap mid kitten\n" +
-      "          heel this ladylike design.",
+    description6: "See-through delicate embroidered organza blue lining luxury acetate-mix stretch pleat detailing. Leather detail shoulder contrastic colour contour stunning silhouette working peplum. Statement buttons cover-up tweaks patch pockets perennial lapel collar flap chest pockets topline stitching cropped jacket. Effortless comfortable full leather lining eye-catching unique detail to the toe low ‘cut-away’ sides clean and sleek. Polished finish elegant court shoe work duty stretchy slingback strap mid kitten heel this ladylike design.",
   },
   es: {
     description1: "Cinta estructurada con cinta de agarre, copas moldeadas invisibles para un ajuste firme, fuerte forro de malla power, detalles deportivos fuertes. La calidez y la comodidad cuelgan holgadamente del cuerpo, un bolsillo grande en la parte delantera, detalles completos de botones, mezcla de algodón, lindos y funcionales. Faldas ceñidas, colores primarios brillantes, paleta llamativa, ambiente de animadora plisada, adornos de rayas. Zapatos de corte básico, tacón de bloque medio grueso, punta almendrada, suela de goma flexible, elegantes y sencillos, detalles metálicos hechos a mano ideales. Sofisticación de pañuelo de bolsillo de seda pura contemporánea, bolsillo con estampado de coral de lujo, patrones inspirados en las tendencias.",
